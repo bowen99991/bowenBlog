09caacb0ecf3bd6ff14851e9cc9e2d6f73a33858
bowen99991 <956125910@qq.com>
2023-09-08 22:07:20 +0800

    change config

---
+++ assets/weberji.html-ad4d3d22.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, e as createStaticVNode } from "./app-b820aa23.js";
+const _imports_0 = "/bowenBlog/assets/image-20230905195333998-77b1546d.png";
+const _imports_1 = "/bowenBlog/assets/image-20230905195833305-45550035.png";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<hr><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>categories 文件包名\ntags  标签名\ntitle 文件标题名\n</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="一-选择题" tabindex="-1"><a class="header-anchor" href="#一-选择题" aria-hidden="true">#</a> 一，选择题</h2><p>1，元素出栈的顺序是先进后出的</p><h3 id="" tabindex="-1"><a class="header-anchor" href="#" aria-hidden="true">#</a></h3><p>2 ，</p><p><img src="' + _imports_0 + '" alt="image-20230905195333998"></p><p>3，数据流图中带有箭头表示的线段是 数据流</p><p>4，需求分析阶段，可以使用的工具是 DFD图 （数据流图）</p><p>5，不属于对象的基本特点是 一致性</p><p>6，一间宿舍可以住多个学生，实体宿舍和学生之间的联系是 一对多</p><p>7，数据管理技术发展的三个阶段中，数据共享最好的是 数据库系统阶段</p><p>8，<strong><img src="' + _imports_1 + '" alt="image-20230905195833305"></strong></p><p>9，web是万维网，是互联网的一部分</p>', 14);
+const _hoisted_15 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_15);
+}
+const weberji_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "weberji.html.vue"]]);
+export {
+  weberji_html as default
+};
