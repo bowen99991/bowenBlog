532c76e1dc7c35c34b0087edc137859a040179ba
bowen99991 <956125910@qq.com>
2023-09-11 22:38:07 +0800

    更改config 配置

---
+++ assets/index.html-65277b1c.js
@@ -0,0 +1,23 @@
+import{_ as e,o as t,c,a as n}from"./app-7de0a309.js";const r={};function s(a,i){return t(),c("div",null,[n(` ## 文档搭建快速开始
+
+**npx** `),n(` 
+\`\`\`bash
+# 初始化，并选择 2.x
+npx @vuepress-reco/theme-cli init
+\`\`\`
+
+**npm**
+
+\`\`\`bash
+# 初始化，并选择 2.x
+npm install @vuepress-reco/theme-cli@1.0.7 -g
+theme-cli init
+\`\`\`
+
+**yarn**
+
+\`\`\`bash
+# 初始化，并选择 2.x
+yarn global add @vuepress-reco/theme-cli@1.0.7
+theme-cli init
+\`\`\` `)])}const o=e(r,[["render",s],["__file","index.html.vue"]]);export{o as default};
---
+++ assets/plugin.html-527681f6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as n,d as s}from"./app-7de0a309.js";const c={},o=s("p",null,"This is plugin.",-1),_=[o];function l(i,r){return t(),n("div",null,_)}const u=e(c,[["render",l],["__file","plugin.html.vue"]]);export{u as default};
