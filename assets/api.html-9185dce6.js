3a12d9cddde8937e72156117b8bd7866ce496077
bowen99991 <956125910@qq.com>
2023-09-11 17:31:47 +0800

    修改路由配置

---
+++ assets/api.html-9185dce6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,d as c}from"./app-158625b6.js";const o={},_=c("p",null,"This is api.",-1),a=[_];function n(i,r){return t(),s("div",null,a)}const d=e(o,[["render",n],["__file","api.html.vue"]]);export{d as default};
