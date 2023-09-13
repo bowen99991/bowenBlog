cfad28e38946b354fb64bec42789032457e87e03
bowen99991 <956125910@qq.com>
2023-09-13 08:47:12 +0800

    0913

---
+++ assets/plugin.html-b8ae8e6e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,b as c}from"./app-eb3e6936.js";const n={},o=c("p",null,"This is api.",-1),_=[o];function l(a,i){return t(),s("div",null,_)}const p=e(n,[["render",l],["__file","plugin.html.vue"]]);export{p as default};
