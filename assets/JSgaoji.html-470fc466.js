7467f86d2c879351daeb52b4ca0db25dd7b1e25f
bowen99991 <956125910@qq.com>
2023-09-15 10:01:32 +0800

    0915 push

---
+++ assets/JSgaoji.html-470fc466.js
@@ -0,0 +1,4 @@
+const data = JSON.parse('{"key":"v-54d7281a","path":"/blogs/JavaScript/JS-gaoji/JSgaoji.html","title":"JS-Dom","lang":"en-US","frontmatter":{"title":"JS-Dom","date":"2023/03/12","tags":["JS-malu"],"categories":["JavaScript/JS-高级"]},"headers":[{"level":2,"title":"一.\\t代码段和作用域","slug":"一-代码段和作用域","link":"#一-代码段和作用域","children":[]},{"level":2,"title":"2.\\t预解析","slug":"_2-预解析","link":"#_2-预解析","children":[]}],"git":{},"filePathRelative":"blogs/JavaScript/JS-高级/JS高级.md"}');
+export {
+  data
+};
