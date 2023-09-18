d464f9be57702b57f70f24f1ce31b34c939bb527
bowen99991 <956125910@qq.com>
2023-09-18 22:25:58 +0800

    0918 push

---
+++ assets/liaojieBootstrap.html-cd7474b0.js
@@ -0,0 +1,93 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, d as createBaseVNode, e as createTextVNode } from "./app-ec369de0.js";
+const _imports_0 = "/bowenBlog/assets/image-20230917213310845-78494f15.png";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode(
+  "p",
+  null,
+  [
+    /* @__PURE__ */ createBaseVNode("img", {
+      src: _imports_0,
+      alt: "image-20230917213310845"
+    })
+  ],
+  -1
+  /* HOISTED */
+);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode(
+  "h2",
+  {
+    id: "一-介绍",
+    tabindex: "-1"
+  },
+  [
+    /* @__PURE__ */ createBaseVNode("a", {
+      class: "header-anchor",
+      href: "#一-介绍",
+      "aria-hidden": "true"
+    }, "#"),
+    /* @__PURE__ */ createTextVNode(" 一, 介绍")
+  ],
+  -1
+  /* HOISTED */
+);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode(
+  "p",
+  null,
+  "官网链接;",
+  -1
+  /* HOISTED */
+);
+const _hoisted_4 = /* @__PURE__ */ createBaseVNode(
+  "p",
+  null,
+  "www.bootcss.com",
+  -1
+  /* HOISTED */
+);
+const _hoisted_5 = /* @__PURE__ */ createBaseVNode(
+  "p",
+  null,
+  "技术文档",
+  -1
+  /* HOISTED */
+);
+const _hoisted_6 = /* @__PURE__ */ createBaseVNode(
+  "p",
+  null,
+  "引入文件 css js, 已经是封装好的组件,",
+  -1
+  /* HOISTED */
+);
+const _hoisted_7 = /* @__PURE__ */ createBaseVNode(
+  "p",
+  null,
+  "可以直接使用",
+  -1
+  /* HOISTED */
+);
+const _hoisted_8 = /* @__PURE__ */ createBaseVNode(
+  "p",
+  null,
+  [
+    /* @__PURE__ */ createBaseVNode("strong", null, "栅格系统")
+  ],
+  -1
+  /* HOISTED */
+);
+const _hoisted_9 = [
+  _hoisted_1,
+  _hoisted_2,
+  _hoisted_3,
+  _hoisted_4,
+  _hoisted_5,
+  _hoisted_6,
+  _hoisted_7,
+  _hoisted_8
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_9);
+}
+const liaojieBootstrap_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "liaojieBootstrap.html.vue"]]);
+export {
+  liaojieBootstrap_html as default
+};
