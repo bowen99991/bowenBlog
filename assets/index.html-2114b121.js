52b56d622dcaf258a33676d746a69071b59d6aae
bowen99991 <956125910@qq.com>
2023-09-20 16:53:35 +0800

    0920 push

---
+++ assets/index.html-2114b121.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createCommentVNode } from "./app-f755b3a9.js";
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, [
+    createCommentVNode(" ## 文档搭建快速开始\n\n**npx** "),
+    createCommentVNode(" \n```bash\n# 初始化，并选择 2.x\nnpx @vuepress-reco/theme-cli init\n```\n\n**npm**\n\n```bash\n# 初始化，并选择 2.x\nnpm install @vuepress-reco/theme-cli@1.0.7 -g\ntheme-cli init\n```\n\n**yarn**\n\n```bash\n# 初始化，并选择 2.x\nyarn global add @vuepress-reco/theme-cli@1.0.7\ntheme-cli init\n``` ")
+  ]);
+}
+const index_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "index.html.vue"]]);
+export {
+  index_html as default
+};
---
+++ assets/qianduanmianshi-HTML.html-073edd84.js
@@ -0,0 +1,40 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createCommentVNode, e as createTextVNode, d as createBaseVNode, b as createStaticVNode } from "./app-f755b3a9.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode(
+  "hr",
+  null,
+  null,
+  -1
+  /* HOISTED */
+);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode(
+  "h3",
+  {
+    id: "_1-doctyple-html-的作用",
+    tabindex: "-1"
+  },
+  [
+    /* @__PURE__ */ createBaseVNode("a", {
+      class: "header-anchor",
+      href: "#_1-doctyple-html-的作用",
+      "aria-hidden": "true"
+    }, "#"),
+    /* @__PURE__ */ createTextVNode(" 1，<! DOCTYPLE html>的作用")
+  ],
+  -1
+  /* HOISTED */
+);
+const _hoisted_3 = /* @__PURE__ */ createStaticVNode("<p>具体而言，<!--DOCTYPE html--> 的作用包括：</p><ol><li>规范浏览器的渲染模式：<!--DOCTYPE html--> 声明指示浏览器使用标准模式（Standards Mode）来解析和渲染页面。标准模式确保页面按照 HTML5 规范进行解析和显示，以保证一致的行为和布局。</li><li>激活最新的 HTML 功能：HTML5 引入了一系列新的功能、元素和属性，包括语义化标签、表单验证、多媒体支持等。通过使用 <!--DOCTYPE html--> 声明，浏览器可以识别并启用这些新功能，以便正确解析和处理页面内容。</li><li>提供向后兼容性：HTML5 规范是对之前 HTML 版本的扩展和改进，通过使用 <!--DOCTYPE html--> 声明，即使在旧版本的浏览器中，也可以提供一定程度的向后兼容性，使页面在各种浏览器中以尽可能一致的方式呈现。</li></ol><p><strong>如果页面没有正确声明 HTML，可能会导致以下问题：</strong></p><ol><li>缺少文档类型声明：缺少 <code>&lt;!DOCTYPE&gt;</code> 声明可能导致浏览器以怪异模式（Quirks Mode）解析页面，这可能引发与标准模式（Standards Mode）下不同的布局和渲染行为。</li><li>字符集未指定：缺少 <code>&lt;meta charset=&quot;...&quot;&gt;</code> 标签可能会导致浏览器无法正确解析和显示页面的字符集，从而导致乱码或显示异常。</li><li>视口设置问题：缺少 <code>&lt;meta name=&quot;viewport&quot; content=&quot;...&quot;&gt;</code> 标签可能导致在移动设备上无法正确缩放和适应屏幕，影响页面在不同设备上的显示效果和用户体验。</li><li>缺少必要的结构和语义：缺少 HTML 标签和正确的语义结构可能使页面结构混乱，无法被搜索引擎正确解析和索引。此外，缺少适当的标记和语义元素也可能导致辅助技术无法正确解读和访问页面内容，影响可访问性。</li><li>缺少必要的标签和属性：例如，缺少 <code>&lt;title&gt;</code> 标签可能导致页面在浏览器标签上显示不友好的标题，同时也会影响搜索引擎对页面主题的理解。</li><li>兼容性问题：缺少 HTML 声明可能导致不同浏览器在解析和呈现页面时存在差异，从而引发兼容性问题。</li></ol>", 4);
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, [
+    _hoisted_1,
+    _hoisted_2,
+    createCommentVNode("DOCTYPE html"),
+    createTextVNode(" 是 HTML 文档类型声明（Document Type Declaration）的一种形式，它的作用是告诉浏览器当前文档使用的是 HTML5 规范。 "),
+    _hoisted_3
+  ]);
+}
+const qianduanmianshiHTML_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "qianduanmianshi-HTML.html.vue"]]);
+export {
+  qianduanmianshiHTML_html as default
+};
