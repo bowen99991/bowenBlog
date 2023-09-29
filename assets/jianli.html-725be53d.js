d20a0e02beb30e605d48e08dd51d75b27f886c6c
bowen99991 <956125910@qq.com>
2023-09-29 13:09:28 +0800

    0929 push

---
+++ assets/jianli.html-725be53d.js
@@ -0,0 +1,4 @@
+const data = JSON.parse('{"key":"v-40e95d92","path":"/blogs/qiuzhi-jianli/jianli.html","title":"简历驱动学习","lang":"en-US","frontmatter":{"title":"简历驱动学习","date":"2022/06/01","tags":["工作求职"],"categories":["求职-简历"]},"headers":[{"level":2,"title":"学习中,做一份简历, 以简历驱动学习","slug":"学习中-做一份简历-以简历驱动学习","link":"#学习中-做一份简历-以简历驱动学习","children":[]},{"level":2,"title":"写好简历","slug":"写好简历","link":"#写好简历","children":[]},{"level":2,"title":"专业技能","slug":"专业技能","link":"#专业技能","children":[]},{"level":2,"title":"项目介绍","slug":"项目介绍","link":"#项目介绍","children":[]}],"git":{},"filePathRelative":"blogs/求职-简历/简历.md"}');
+export {
+  data
+};
