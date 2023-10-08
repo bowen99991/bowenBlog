7b0c066c28b230c1bccef72c6bc17eb6749f4582
bowen99991 <956125910@qq.com>
2023-10-08 22:33:29 +0800

    1008 push

---
+++ assets/Ajax.html-6a50037d.js
@@ -0,0 +1,4 @@
+const data = JSON.parse('{"key":"v-6508bed6","path":"/blogs/JavaScript/Ajax/Ajax.html","title":"Ajax","lang":"en-US","frontmatter":{"title":"Ajax","date":"2023/03/27","tags":["Ajax"],"categories":["JavaScript/Ajax"]},"headers":[{"level":2,"title":"Ajax 介绍","slug":"ajax-介绍","link":"#ajax-介绍","children":[]},{"level":2,"title":"express 框架的使用","slug":"express-框架的使用","link":"#express-框架的使用","children":[]},{"level":2,"title":"Ajax 使用 get 传参","slug":"ajax-使用-get-传参","link":"#ajax-使用-get-传参","children":[]},{"level":2,"title":"使用 post 传参","slug":"使用-post-传参","link":"#使用-post-传参","children":[]}],"git":{},"filePathRelative":"blogs/JavaScript/Ajax/Ajax.md"}');
+export {
+  data
+};
