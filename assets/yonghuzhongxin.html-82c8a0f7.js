6f285b6df8e319a6f16b77984e5e8b4169b10216
bowen99991 <956125910@qq.com>
2023-10-10 08:54:14 +0800

    1009 push

---
+++ assets/yonghuzhongxin.html-82c8a0f7.js
@@ -0,0 +1,28 @@
+import { _ as _export_sfc, r as resolveComponent, o as openBlock, c as createElementBlock, b as createBaseVNode, d as createTextVNode, e as createVNode, a as createStaticVNode } from "./app-cc3f963f.js";
+const _imports_0 = "/bowenBlog/assets/image-20230929170036846-9ffe559d.png";
+const _imports_1 = "/bowenBlog/assets/image-20230929165958726-a3935a58.png";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<hr><h2 id="企业做项目流程" tabindex="-1"><a class="header-anchor" href="#企业做项目流程" aria-hidden="true">#</a> 企业做项目流程:</h2><p>需求分析 =&gt; 设计 =&gt; 技术选型 =&gt;</p><p>初始化 =&gt; 写代码(实现业务逻辑 ) =&gt; 测试(单元测试) =&gt; 代码提交 / 代码评审 =&gt; 部署 =&gt; 发布</p><h2 id="需求分析" tabindex="-1"><a class="header-anchor" href="#需求分析" aria-hidden="true">#</a> 需求分析</h2><ol><li>登录 / 注册</li><li>用户管理 (管理员的权限 ) 对用户的查询或修改</li><li>用户校验 ( .... 对特定的用户)</li></ol><h2 id="技术选型" tabindex="-1"><a class="header-anchor" href="#技术选型" aria-hidden="true">#</a> 技术选型</h2><p>前端 : HTML CSS JS Ant Design + Umi + Ant Design Pro</p><p>后端 : Java + spring + springmvc mybatis + mybatis-plus + springboot + mysql</p><p>部署</p><h2 id="计划" tabindex="-1"><a class="header-anchor" href="#计划" aria-hidden="true">#</a> 计划</h2><ol><li>初始化项目 (准备环境) <ol><li>前端 <ol><li>初始化</li><li>引入组件</li><li>项目瘦身( ) ==框架介绍==</li></ol></li><li>后端 <ol><li>准备环境</li><li>引入框架</li></ol></li></ol></li><li>登录 / 注册</li><li>用户管理( 管理员可见)</li></ol><p><strong>前端框架</strong></p>', 13);
+const _hoisted_14 = {
+  href: "https://pro.ant.design/zh-CN/docs/getting-started",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_15 = /* @__PURE__ */ createStaticVNode('<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>npx create-umi myapp \n\nyarn create umi myapp\n\n\n安装yarn \n\nnpm install -g yarn\n\n\nyarn create umi myapp xxx\nnpm run dev 启动项目\n</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>项目安装架构</p><p><img src="' + _imports_0 + '" alt="image-20230929170036846"></p><p><img src="' + _imports_1 + '" alt="image-20230929165958726"></p>', 4);
+function _sfc_render(_ctx, _cache) {
+  const _component_ExternalLinkIcon = resolveComponent("ExternalLinkIcon");
+  return openBlock(), createElementBlock("div", null, [
+    _hoisted_1,
+    createBaseVNode("p", null, [
+      createBaseVNode("a", _hoisted_14, [
+        createTextVNode("开始使用 - Ant Design Pro"),
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ]),
+    _hoisted_15
+  ]);
+}
+const yonghuzhongxin_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "yonghuzhongxin.html.vue"]]);
+export {
+  yonghuzhongxin_html as default
+};
