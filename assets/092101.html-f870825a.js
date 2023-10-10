ac3a44dc69dc98783c067eae724f3cbbe17984e5
bowen99991 <956125910@qq.com>
2023-10-10 20:43:12 +0800

    1010 push

---
+++ assets/092101.html-f870825a.js
@@ -0,0 +1,19 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createBaseVNode } from "./app-44d54ae0.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode(
+  "p",
+  null,
+  "second page in category2",
+  -1
+  /* HOISTED */
+);
+const _hoisted_2 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_2);
+}
+const _092101_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "092101.html.vue"]]);
+export {
+  _092101_html as default
+};
