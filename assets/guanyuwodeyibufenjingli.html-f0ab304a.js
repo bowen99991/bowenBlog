1a34b222fa92b6eca1f378f3fdc41cd27b128a53
Bowen <956125910@qq.com>
2023-12-17 23:10:42 +0800

    update 更新博客

---
+++ assets/guanyuwodeyibufenjingli.html-f0ab304a.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, d as createStaticVNode } from "./app-7a52d7d0.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<hr><h2 id="_2020-12-01" tabindex="-1"><a class="header-anchor" href="#_2020-12-01" aria-hidden="true">#</a> 2020/12/01</h2><p>结束也是开始...</p><p>2018.09.11 - 2020.12.01</p><h2 id="个人介绍" tabindex="-1"><a class="header-anchor" href="#个人介绍" aria-hidden="true">#</a> 个人介绍</h2><p>博主：麦田守望者</p><p>性别：男</p><p>爱好：唱歌、跑步 、篮球、健身、摄影、徒步...</p><p>梦想：成为计算机领域的大佬</p><p>我的性格&amp;三观</p><p>性格：</p><ul><li>性格温和，三观正常，心态平和，不情绪化</li><li>内心强大，有进取心，热爱生活，注重细节</li></ul><p>三观</p><ul><li>世界观：注重长期，永远持有包容开放的心态</li><li>人生观：人生是场体验，多追求不同的体验</li><li>价值观：要结果，要反馈，要付出，要收获</li></ul><h2 id="我喜欢什么" tabindex="-1"><a class="header-anchor" href="#我喜欢什么" aria-hidden="true">#</a> 我喜欢什么？</h2><ul><li>喜欢蓝色, 喜欢清新的颜色, 喜欢简约的颜色, 喜欢舒适的颜色</li><li>喜欢被别人夸赞的感觉，喜欢尝试新事物</li><li>喜欢唱歌，喜欢听各种好听的歌</li><li>喜欢吃美食，喜欢旅游，喜欢探索</li><li>喜欢研究技术，喜欢和别人讨论技术</li><li>喜欢学习知识，喜欢解决问题的成就感</li><li>喜欢有个性的东西，喜欢做有意思的事情</li></ul>', 16);
+const _hoisted_17 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_17);
+}
+const guanyuwodeyibufenjingli_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "guanyuwodeyibufenjingli.html.vue"]]);
+export {
+  guanyuwodeyibufenjingli_html as default
+};
---
+++ assets/ruhexuanzeyifengongzuo.html-400a1e2a.js
@@ -0,0 +1,58 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createBaseVNode, b as createTextVNode } from "./app-7a52d7d0.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode(
+  "hr",
+  null,
+  null,
+  -1
+  /* HOISTED */
+);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode(
+  "h2",
+  {
+    id: "您对工作有什么看法",
+    tabindex: "-1"
+  },
+  [
+    /* @__PURE__ */ createBaseVNode("a", {
+      class: "header-anchor",
+      href: "#您对工作有什么看法",
+      "aria-hidden": "true"
+    }, "#"),
+    /* @__PURE__ */ createTextVNode(" 您对工作有什么看法?")
+  ],
+  -1
+  /* HOISTED */
+);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode(
+  "ol",
+  null,
+  [
+    /* @__PURE__ */ createBaseVNode("li", null, [
+      /* @__PURE__ */ createBaseVNode("p", null, "公司的平台和未来发展：你提到这是你最看重的因素之一。你可以研究一下这三家公司的发展前景、行业地位以及他们未来的战略规划。考虑公司的发展前景是否与你的职业目标相符，以及他们的平台是否能够提供你成长的机会。")
+    ]),
+    /* @__PURE__ */ createBaseVNode("li", null, [
+      /* @__PURE__ */ createBaseVNode("p", null, "工作内容和技术栈：对于前端工程师来说，工作内容和技术栈至关重要。你可以了解一下在这三家公司的前端团队中会有哪些技术栈和项目。你对哪些技术感兴趣？你希望在哪个领域深耕？在选择之前，搞清楚你将在工作中遇到什么样的挑战，以及你是否对此感兴趣。")
+    ]),
+    /* @__PURE__ */ createBaseVNode("li", null, [
+      /* @__PURE__ */ createBaseVNode("p", null, "工作环境和文化：工作的环境和文化对于你的工作体验和满意度有很大影响。你可以尝试了解一下这三家公司的工作氛围、团队文化和员工福利。你喜欢一个有活力的工作环境还是更喜欢一个稳定且注重工作生活平衡的文化？")
+    ]),
+    /* @__PURE__ */ createBaseVNode("li", null, [
+      /* @__PURE__ */ createBaseVNode("p", null, "薪水和福利：虽然你提到钱是其次的考虑因素，但也是不可忽视的一点。你可以对比一下这三份offer提供的薪水、福利和其他额外的补贴，以确保它们能够满足你的经济需求。")
+    ])
+  ],
+  -1
+  /* HOISTED */
+);
+const _hoisted_4 = [
+  _hoisted_1,
+  _hoisted_2,
+  _hoisted_3
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_4);
+}
+const ruhexuanzeyifengongzuo_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "ruhexuanzeyifengongzuo.html.vue"]]);
+export {
+  ruhexuanzeyifengongzuo_html as default
+};
