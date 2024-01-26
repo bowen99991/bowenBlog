d97027cee0446590edee1f3d328dd0c8baef5dd9
hubowen <956125910@qq.com>
2024-01-26 13:10:53 +0800

    update:更新博客

---
+++ assets/SSM-MybatisPlus.html-56846ea1.js
@@ -0,0 +1,14 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, d as createStaticVNode } from "./app-0bcd8dca.js";
+const _imports_0 = "/bowenBlog/assets/image-20240113104419670-cc9b75cb.png";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<hr><h2 id="一-介绍" tabindex="-1"><a class="header-anchor" href="#一-介绍" aria-hidden="true">#</a> 一, 介绍</h2><hr><p>MyBatis和MyBatis-Plus是两个在Java开发中常用的<strong>持久层框架</strong>，它们都与数据库交互，并提供了一种简化和优化数据库访问操作的方式。</p><p><strong>图解介绍</strong></p><p><img src="' + _imports_0 + '" alt="image-20240113104419670"></p><p><strong>增强与改进</strong></p><p>MyBatis-Plus是在MyBatis的基础上进行扩展和增强的框架，提供了更多便捷的开发功能和增强的操作能力。它通过提供一系列的代码生成器、通用CRUD方法、分页插件、性能分析插件等功能，简化了开发过程，提高了开发效率。MyBatis-Plus还提供了更加方便的查询构建器，支持<strong>Lambda表达式</strong>和<strong>链式调用</strong>等方式，使得查询操作更加简洁和可读性更好。</p>', 8);
+const _hoisted_9 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", null, _hoisted_9);
+}
+const SSMMybatisPlus_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "SSM-MybatisPlus.html.vue"]]);
+export {
+  SSMMybatisPlus_html as default
+};
---
+++ assets/SSM-Redis.html-bb94d546.js
@@ -0,0 +1,4 @@
+const data = JSON.parse('{"key":"v-5477d1d0","path":"/docs/Java/SSM-Redis.html","title":"SSM-Redis","lang":"en-US","frontmatter":{"title":"SSM-Redis","date":"2021/01/15","tags":["Java"],"categories":null},"headers":[{"level":2,"title":"一,\\tRedis 简介","slug":"一-redis-简介","link":"#一-redis-简介","children":[{"level":3,"title":"1,\\t什么是 redis","slug":"_1-什么是-redis","link":"#_1-什么是-redis","children":[]}]}],"git":{"createdTime":null,"updatedTime":null,"contributors":[]},"filePathRelative":"docs/Java/SSM-Redis.md"}');
+export {
+  data
+};
